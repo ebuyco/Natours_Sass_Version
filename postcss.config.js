0aa12709d702c5741f19115776b55f0c1d76e65d
ErnieBuyco <buycoernie@gmail.com>
2020-05-07 17:41:28 +0800

    test

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-import'),
+    require('postcss-preset-env'),
+  ],
+};
+// module.exports = {
+//     'postcss-import': {},
+//     'postcss-preset-env': {},
+//     'autoprefixer': {},
+// };
